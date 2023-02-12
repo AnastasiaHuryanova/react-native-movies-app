62e59dec95db69690925b0ce923aab1f541bb2fd
Anastasia Hur'yanova <anastasia39@hotmail.it>
2023-02-12 02:40:56 +0100

    create movie detail screen

--- src/screens/MovieDetail.jsx
+++ src/screens/MovieDetail.jsx
@@ -1,10 +1,31 @@
-import {Text, View} from 'react-native';
+import {useEffect, useState} from 'react';
+import {Image, Text, View} from 'react-native';
+import {getMovieDetail} from '../axios/theMovieDb/movies';
+
+const TMDB_URL = 'https://image.tmdb.org/t/p/w500';
 
 const MovieDetail = ({route}) => {
   const {id} = route.params;
+
+  const [details, setDetails] = useState({});
+
+  useEffect(() => {
+    const fetchMovieDetail = async () => {
+      const movieDetail = await getMovieDetail(id);
+      setDetails(movieDetail);
+    };
+
+    fetchMovieDetail();
+  }, []);
+
   return (
     <View>
-      <Text>{id}</Text>
+      <Text>{details.title}</Text>
+      <Image
+        style={{height: 300}}
+        source={{uri: TMDB_URL + details.backdrop_path}}
+      />
+      <Text>{details.overview}</Text>
     </View>
   );
 };
